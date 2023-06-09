11660a52774164d50bdc98606eb0099829b4b44b
Ramon Matute <ramonmatute2003@gmail.com>
2023-06-09 13:29:25 -0600

    Initial commit: 2023/06/09 13:25

--- app/src/main/java/com/example/pmo1_exercise12/MainActivity.java
+++ app/src/main/java/com/example/pmo1_exercise12/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pmo1_exercise12;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.PatternsCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -27,20 +28,30 @@ public class MainActivity extends AppCompatActivity {
 
         btn_send.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 if(txt_name.getText().toString().equals("") || txt_last_name.getText().toString().equals("") || txt_age.getText().toString().equals("") || txt_email.getText().toString().equals("")){
                     Toast.makeText(getApplicationContext(), "No dejar campos vacios",Toast.LENGTH_LONG).show();
                 }else{
-                    Information information=new Information(txt_name.getText().toString(), txt_last_name.getText().toString(), (Integer.parseInt(txt_age.getText().toString())), txt_email.getText().toString());
-                    Intent new_window=new Intent(getApplicationContext(), Message_Activity.class);
-                    new_window.putExtra("name",information.getName());
-                    new_window.putExtra("last_name",information.getLast_name());
-                    new_window.putExtra("age",information.getAge());
-                    new_window.putExtra("email",information.getEmail());
-                    startActivity(new_window);
+                    if(validate_email(txt_email.getText().toString())==true){
+                        Information information=new Information(txt_name.getText().toString(), txt_last_name.getText().toString(), (Integer.parseInt(txt_age.getText().toString())), txt_email.getText().toString());
+                        Intent new_window=new Intent(getApplicationContext(), Message_Activity.class);
+                        new_window.putExtra("name",information.getName());
+                        new_window.putExtra("last_name",information.getLast_name());
+                        new_window.putExtra("age",information.getAge());
+                        new_window.putExtra("email",information.getEmail());
+                        startActivity(new_window);
+                    }
                 }
-
             }
         });
+    }
+
+    private boolean validate_email(String email){
+        if(!PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(getApplicationContext(), "Por favor escribe el correo valido",Toast.LENGTH_LONG).show();
+            return false;
+        }else{
+            return true;
+        }
     }
 }
